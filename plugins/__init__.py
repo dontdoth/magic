968ca61c1c4dcb099641f3791a8af9d10f9a7403
DEVA <divyanshuchaudhary482@gmail.com>
2025-01-25 19:50:46 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("BRANDEDKING82")
+    return web.json_response("its_deva_heree")
 
 
 async def web_server():
